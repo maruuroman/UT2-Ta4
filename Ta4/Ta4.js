4fbbff211c719da765b5ebbf39ae6c2c26f674ee
maruuroman <141636654+maruuroman@users.noreply.github.com>
2024-08-17 11:49:59 -0300

    Add files via upload

---
+++ Ta4/Ta4.js
@@ -0,0 +1,15 @@
+function sumAll(a, b){
+    
+    let resultado = 0;
+
+    for (let i = a; i <= b; i++) {
+        resultado += i;
+        
+    }
+
+
+
+    return resultado;
+}
+
+console.log(sumAll(3, 5));
